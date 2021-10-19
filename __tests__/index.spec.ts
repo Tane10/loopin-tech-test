3f19a452a016e7c7149f6660f2c54573901d77bc
Tane10 <tane2@hotmail.co.uk>
2021-10-19 17:06:52 +0100

    moved it to classes cause thats how i think now

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,18 +1,8 @@
-import { findNextWorkingDay } from "../src/index";
-
-// - Is 🎅**Christmas** 🎄removed from any working week? -> important
-// - Is ✨**New Years Day** ✨ removed from any working week? -> important
-// - Do we only include dates if they fall within Monday to Friday? -> -> important
-// - Does the function return a maximum of five dates? -> important
-// - If a holiday falls on a working week, do we remove it from the returned array? -> important
-
-// - Are we checking holidays in a way where it would be easy to add new holidays in the future?
-// - Are the dates set to 8:30am locale `en` time?
+import FindNextWorkingDays from "../src/index";
+import { selectedHolidays, ukDataFormate } from "./mock.data";
 
 describe("Find next working day", () => {
-  test("Should accept todays date and return a max of the next 5 working days", () => {
-      
-  });
+  test("Should accept todays date and return a max of the next 5 working days", () => {});
 
   test("Should remove christmas from any working week", () => {});
 
---
+++ __tests__/mock.data.ts
@@ -0,0 +1,3 @@
+export const selectedHolidays: string[] = ["25-12-2021", "01-01-22"];
+
+export const ukDataFormate = "DD/MM/YY";
